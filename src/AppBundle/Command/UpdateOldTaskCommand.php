0e4ed9a0e7a929214e6825b93f27b15864a27e28
Benj972 <ben.gallot972@gmail.com>
2018-07-12 18:49:31 +0100

    implement command for user anonymous

---
+++ src/AppBundle/Command/UpdateOldTaskCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Task;
+
+class UpdateOldTaskCommand extends ContainerAwareCommand
+{
+    private $manager;
+
+    private $encoder;
+
+    public function __construct(EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        parent::__construct();
+        $this->manager = $manager;
+        $this->encoder = $encoder;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('demo:load')
+            ->setDescription('Updates old tasks without author')
+            ->addArgument('anonymous')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $usercommand = $input->getArgument('anonymous');
+        $text = strtoupper('Bad Command');
+        if (true == $usercommand) {
+            $anonymous = $this->load();
+            $this->setOldTask($anonymous);
+            $text = strtoupper('Old Task Update');
+        }
+        $output->writeln($text);
+    }
+
+    public function load()
+    {
+        $anonymous = new User();
+        $anonymous->setUsername('anonymous');
+        $plainPassword = 'passwordanonymous';
+        $encoded = $this->encoder->encodePassword($anonymous, $plainPassword);
+        $anonymous->setPassword($encoded);
+        /*$anonymous->setPassword('password');*/
+        $anonymous->setEmail('anonymous@hotmail.fr');
+        $anonymous->setRoles(['ROLE_USER']);
+        $this->manager->persist($anonymous);
+        $this->manager->flush();
+        return $anonymous;
+    }
+
+    public function setOldTask($anonymous)
+    {
+        $tasks = $this->manager->getRepository('AppBundle:Task')->findAll();
+        foreach ($tasks as $task) {
+            if (null === $task->getUser()) {
+                $task->setUser($anonymous);
+                $this->manager->persist($task);
+                $this->manager->flush();
+            }
+        }
+    }
+}
