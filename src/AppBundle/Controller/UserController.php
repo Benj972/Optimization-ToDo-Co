3b70eaf013dc1cd7f730c487c199ceeed8e5e0b8
Benj972 <ben.gallot972@gmail.com>
2018-07-19 08:48:52 +0100

    modify management encode password with UserSubscriber.php

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -34,8 +34,8 @@ class UserController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
+            /*$password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);*/
 
             $em->persist($user);
             $em->flush();
@@ -58,8 +58,8 @@ class UserController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
+            /*$password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);*/
 
             $this->getDoctrine()->getManager()->flush();
 
